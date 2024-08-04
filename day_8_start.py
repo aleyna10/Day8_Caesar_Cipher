c4c929881de765e260279ba5cac559b4c5f61306
aleyna <98637878+aleyna10@users.noreply.github.com>
2024-08-04 13:39:22 +0200

    Add files via upload

---
+++ day_8_start.py
@@ -0,0 +1,17 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+# def greet():
+#   print("Hello")
+#   print("Dag")
+#   print("Tot ziens")
+
+# greet()
+
+def greet_with(name, location):
+  print(f"Hello, {name}!")
+  print(f"What is it in     location?")
+
+greet_with(location="Nihel",name="LA")
+
